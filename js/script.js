cc2b8c4030381fc236750fb6eee956a56e08af82
Roberto Riccioli <riccioli.rob@gmail.com>
2020-07-08 11:22:36 +0200

    aggiunta dropdown

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+console.log("hello world");
+
 $(document).ready(function() {
 
-  $(".dropdown").mouseenter(function() {
+  $(".flex-links .dropdown").mouseenter(function() {
     $(this).children(".drop-list").show();
   });
 
-  $(".dropdown").mouseleave(function() {
+  $(".flex-links .dropdown").mouseleave(function() {
     $(this).children(".drop-list").hide();
   });
 
